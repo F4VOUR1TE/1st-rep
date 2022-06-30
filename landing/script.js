0b322e9aca53bd4d50d563e15bb70f9fabba82da
Илья <i-hawk18@mail.ru>
2022-06-30 16:23:42 +0300

    +js +дополнили html

---
+++ landing/script.js
@@ -0,0 +1,31 @@
+// function Habibi (name, age) {
+    // alert(`Добро пожаловать на сайт, ${name}, у нас есть предложения даже для ваших(${age}) лет!`)
+// }
+// Habibi('Илья', 25)
+
+
+
+// age = prompt('Ваш возраст')
+// age > 18 ? alert('gay') : alert('poop')
+ 
+
+
+// for(let i = 0; i<10; i++){
+    // alert(i)
+//  }
+
+// let arr = [10,11,12,13,14,15]
+
+
+
+// arr.map(
+    // (item)=>{
+        // alert(item)
+    // }
+// )
+
+let title = document.getElementsByClassName('van-bg-text')
+
+console.log(title)
+
+// title.innerHTML = 'HEllo from JS'
